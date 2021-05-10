22c418a5b8981d6f653dcff98d9ad35dfc8f4e23
you-5805 <you.iwamoto918@gmail.com>
2021-05-10 14:25:43 +0900

    update user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,11 +2,13 @@ class Api::V1::UsersController < ApiController
   def create
     @user = User.create(user_params)
     response_bad_request if !@user
+    JwtAuth.tokenize(params[:user][:uid])
+    response.set_header('Access-Token', token)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:uid, :name)
+    params.permit(:uid, :name)
   end
 end
