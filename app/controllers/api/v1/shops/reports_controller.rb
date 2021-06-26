82cc49e69cbe55985198e47a4836daa062844177
you-5805 <you.iwamoto918@gmail.com>
2021-06-26 18:39:00 +0900

    fix

--- app/controllers/api/v1/shops/reports_controller.rb
+++ app/controllers/api/v1/shops/reports_controller.rb
@@ -1,14 +1,11 @@
 class Api::V1::Shops::ReportsController < ApiController
   def create
-    current_user = User.first
     shop = Shop.where(hotpepper_id: params[:shop_hotpepper_id]).first || Shop.create(hotpepper_id: params[:shop_hotpepper_id])
     begin
-      return unless current_user.reports.create(shop_id: shop[:id])
-
-      response_bad_request(like.errors.messages)
+      shop.reports.create(user_id: current_user.id)
     rescue ActiveRecord::RecordNotUnique
     end
-    result = ShopsHandler.getAPIResult({ id: shop[:hotpepper_id] }, current_user)
+    result = ShopsHandler.getAPIResult({ id: shop.hotpepper_id }, current_user)
     @shop = result[:shop][0]
   end
 
@@ -18,8 +15,8 @@ class Api::V1::Shops::ReportsController < ApiController
     rescue ActiveRecord::RecordNotFound
       response_bad_request('record not found')
     end
-    if shop & shop.isReportedBy(current_user)
-      shop.reports.where(user_id: current_user[:id]).destroy
+    if !!shop & shop.isReportedBy(current_user)
+      shop.reports.find_by!(user_id: current_user[:id]).destroy
     end
     result = ShopsHandler.getAPIResult({ id: shop[:hotpepper_id] }, current_user)
     @shop = result[:shop][0]
